5d65fdc58a31d46a0b6ccefaab25ee6019afae03
César Alejandro Montaño Cortés <cesarin_mon@hotmail.com>
2018-05-24 12:40:31 -0500

    Poligono, Modificacion para BD

---
+++ hackathon-transparencia/grails-app/domain/com/sigueme/Poligonos.groovy
@@ -0,0 +1,13 @@
+package com.sigueme
+
+class Poligonos {
+
+	String nombre
+	String entidad
+	Integer claveEntidad
+	Integer clavePoligono
+	TipoPoligono tipoPoligono
+
+    static constraints = {
+    }
+}
