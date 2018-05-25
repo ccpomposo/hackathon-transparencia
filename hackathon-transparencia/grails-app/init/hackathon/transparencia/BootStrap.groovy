e95adf6804e47b9c5d48a9721cca82d64c7aec26
César Alejandro Montaño Cortés <cesarin_mon@hotmail.com>
2018-05-24 19:23:42 -0500

    Pantalla Contactenos, Mas Controller

--- hackathon-transparencia/grails-app/init/hackathon/transparencia/BootStrap.groovy
+++ hackathon-transparencia/grails-app/init/hackathon/transparencia/BootStrap.groovy
@@ -1,9 +1,22 @@
 package hackathon.transparencia
 
+import com.sigueme.*;
+
 class BootStrap {
 
     def init = { servletContext ->
+    	def rolAdmin = new Rol(authority: 'ROLE_ADMIN').save()
+
+    	def usuAdmin = new Usuario(username: 'admin', password: 'admin').save()
+
+    	UsuarioRol.create usuAdmin, rolAdmin
+
+    	UsuarioRol.withSession {
+    		it.flush()
+    		it.clear()
+    	}
     }
+
     def destroy = {
     }
 }
